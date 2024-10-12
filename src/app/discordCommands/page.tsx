5b50828674acafd1e4a4e6e9d046fb0962619dc9
Leul Mesfin <leulmesfin152025@gmail.com>
2024-10-11 20:45:05 -0400

    Search feature ui done

--- src/app/discordCommands/page.tsx
+++ src/app/discordCommands/page.tsx
@@ -2,92 +2,85 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input"
-import { Search } from 'lucide-react';
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
 
 const discordCommands = () => {
 	return (
 		<div>
 			<Navbar />
 			<div className="bg-slate-600 h-full w-full">
-				<h1 className="text-4xl font-bold text-white text-center">Commands:</h1>
-        
-        <div>
-          <Search />
-          <Input />
+				<h1 className="text-4xl font-bold text-white text-center pt-4 pb-4">Commands:</h1>
+
+				<div className="relative max-w-sm mx-auto">
+          <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600 h-4 w-4 " />
+          <Input
+            type='search'
+            placeholder='Search commands...'
+            className="pl-10 pr-4 bg-gray-50 rounded-md"
+          />
         </div>
-        
-        {/* All discord commands */}
+
+				{/* All discord commands */}
 				<div className="flex flex-col items-center pt-5 gap-4 pb-5">
-          
 					<Card className="text-center w-64">
 						<CardHeader>
 							<CardTitle>$nfl_scores</CardTitle>
 						</CardHeader>
 						<CardContent>
 							<p>
-                Displays the current week's NFL scores.
-                It also displays the date and channel of each game.
+								Displays the current week's NFL scores. It also displays the
+								date and channel of each game.
 							</p>
 						</CardContent>
 					</Card>
 
-          <Card className="text-center w-64">
+					<Card className="text-center w-64">
 						<CardHeader>
 							<CardTitle>$bye_week</CardTitle>
 						</CardHeader>
 						<CardContent>
-							<p>
-                Displays all the NFL teams currently on a bye-week.
-							</p>
+							<p>Displays all the NFL teams currently on a bye-week.</p>
 						</CardContent>
 					</Card>
 
-          <Card className="text-center w-64">
+					<Card className="text-center w-64">
 						<CardHeader>
 							<CardTitle>$rushing_yards</CardTitle>
 						</CardHeader>
 						<CardContent>
-							<p>
-                Displays a leaderboard of the current week's rushing yards.
-							</p>
+							<p>Displays a leaderboard of the current week's rushing yards.</p>
 						</CardContent>
 					</Card>
 
-          <Card className="text-center w-64">
+					<Card className="text-center w-64">
 						<CardHeader>
 							<CardTitle>$passing_yards</CardTitle>
 						</CardHeader>
 						<CardContent>
-							<p>
-                Displays a leaderboard of the current week's passing yards.
-							</p>
+							<p>Displays a leaderboard of the current week's passing yards.</p>
 						</CardContent>
 					</Card>
 
-          <Card className="text-center w-64">
+					<Card className="text-center w-64">
 						<CardHeader>
 							<CardTitle>$receiving_yards</CardTitle>
 						</CardHeader>
 						<CardContent>
 							<p>
-                Displays a leaderboard of the current week's receiving yards.
+								Displays a leaderboard of the current week's receiving yards.
 							</p>
 						</CardContent>
 					</Card>
 
-          <Card className="text-center w-64">
+					<Card className="text-center w-64">
 						<CardHeader>
 							<CardTitle>$aura_rank</CardTitle>
 						</CardHeader>
 						<CardContent>
-							<p>
-                Displays a leaderboard of the current week's Aura ranking.
-							</p>
+							<p>Displays a leaderboard of the current week's Aura ranking.</p>
 						</CardContent>
 					</Card>
-
-
 				</div>
 			</div>
 			<Footer />
